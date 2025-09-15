15d84ba8ac4f468b1d8c3d4b1c84d03eb6c56dd6
j-class <g80047062@gmail.com>
2025-09-15 04:28:38 -0700

    inetial commit

---
+++ converter.js
@@ -0,0 +1,7 @@
+function toRoman (num) {
+    const vals = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+const romans = ["M","CM","D","CD","C","XC","L","XL","X"]
+
+
+
+}
---
+++ temp.js
@@ -0,0 +1,9 @@
+let array =[] 
+for (let i = 0 ; i<11 ; i++) {
+    array.push(i)
+}
+console.log (array)
+
+// let solid =
+
+// for ()
